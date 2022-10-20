0d1a6e675bbdcd6d13357abe465f625f1a4a90c3
N.SH <sky.blush@ya.ru>
2022-10-20 21:41:39 +0300

    first task

--- 01-SumOfPositive/index.js
+++ 01-SumOfPositive/index.js
@@ -1,7 +1,12 @@
 // https://www.codewars.com/kata/5715eaedb436cf5606000381
 
 function positiveSum(arr) {
-    // Code here
+    let sum = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) sum += arr[i]
+    }
+    return sum
 }
 
 module.exports = positiveSum
