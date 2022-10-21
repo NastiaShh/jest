61bf7c6d1407377c56afbbba2cca879150d67ef4
N.SH <sky.blush@ya.ru>
2022-10-21 13:19:23 +0300

    third task

--- 05-PrepareTheCountOfLanguages/index.js
+++ 05-PrepareTheCountOfLanguages/index.js
@@ -1,7 +1,9 @@
 // https://www.codewars.com/kata/5828713ed04efde70e000346/train/javascript
 
 function countLanguages(list) {
-    // Code here
+    let res = {};
+    list.forEach(developer => res[developer.language] = (res[developer.language] || 0) + 1);
+    return res;
 }
 
-module.exports = countLanguages
+module.exports = countLanguages;
