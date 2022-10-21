3b663eb7620a0fb75e5a4ebc9cf42f0902169422
N.SH <sky.blush@ya.ru>
2022-10-21 13:20:10 +0300

    second task

--- 04-RemoveFirstAndLastCharacter/index.js
+++ 04-RemoveFirstAndLastCharacter/index.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 
-const removeChar = (str) => str.slice(1, str.length - 1);
+const removeChar = str => str.slice(1, -1);
 
-module.exports = removeChar
+module.exports = removeChar;
