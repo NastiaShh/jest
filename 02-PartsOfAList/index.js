a399c1c7f8e6425aa0b55ffc1d9b2664c39dba99
N.SH <sky.blush@ya.ru>
2022-11-02 21:02:48 +0300

    delete unnessesary dirs

--- 02-PartsOfAList/index.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/56f3a1e899b386da78000732/train/javascript
-
-function partlist(arr) {
-    // your code
-}
-
-module.exports = partlist
--- 03-IsSatorSquare/index.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/5cb7baa989b1c50014a53333/train/javascript
-
-function isSatorSquare(tablet) {
-    // Code here
-}
-
-module.exports = isSatorSquare
